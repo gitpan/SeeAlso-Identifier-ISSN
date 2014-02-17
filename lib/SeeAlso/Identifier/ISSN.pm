93915662921f9407db0a173d776abc6c8739b4d0
Thomas Berger <ThB@gymel.com>
2014-02-17 20:08:18 +0000

    Import of THB/SeeAlso-Identifier-ISSN-0.5704 from CPAN.
    gitpan-cpan-distribution: SeeAlso-Identifier-ISSN
    gitpan-cpan-version:      0.5704
    gitpan-cpan-path:         THB/SeeAlso-Identifier-ISSN-0.5704.tar.gz
    gitpan-cpan-author:       THB
    gitpan-cpan-maturity:     released

--- lib/SeeAlso/Identifier/ISSN.pm
+++ lib/SeeAlso/Identifier/ISSN.pm
@@ -5,7 +5,7 @@ use warnings;
 BEGIN {
     use Exporter ();
     use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-    $VERSION     = '0.5703';
+    $VERSION     = '0.5704';
     @ISA         = qw(Exporter);
     #Give a hoot don't pollute, do not export more than needed by default
     @EXPORT      = qw();
